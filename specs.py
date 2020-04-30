1f1bb6acf107ed971c0279e78d21b69c4e004e8a
Yonatan Most <>
2020-04-30 12:41:58 +0300

    Increase spec level

--- specs.py
+++ specs.py
@@ -8,13 +8,14 @@ from worksheet_spec import WorksheetSpec
 SPECS: Tuple[WorksheetSpec, ...] = (
     WorksheetSpec('ages 5-6', (
         (1, ExerciseTemplate(Operation.addition, InRange(0, 15), InRange(0, 15), InRange(0, 30))),
-        (1, ExerciseTemplate(Operation.subtraction, InRange(0, 10), InRange(0, 10), InRange(0, 10))),
-        (0.4, ExerciseTemplate(Operation.subtraction, InRange(0, 9), InRange(0, 10), InRange(-9, -1))),
+        (1, ExerciseTemplate(Operation.subtraction, InRange(0, 15), InRange(0, 10), InRange(0, 15))),
+        (0.4, ExerciseTemplate(Operation.subtraction, InRange(0, 10), InRange(0, 11), InRange(-10, -1))),
         (0.4, ExerciseTemplate(Operation.multiplication, InRange(0, 5), InRange(0, 5), InRange(0, 25))),
-        (0.2, ExerciseTemplate(Operation.division, Even(InRange(0, 16)), InRange(2, 2), NoConstraint()))
+        (0.2, ExerciseTemplate(Operation.division, Even(InRange(0, 20)), InRange(2, 2), NoConstraint()))
     )),
     WorksheetSpec('ages 3-4', (
-        (1, ExerciseTemplate(Operation.addition, InRange(0, 9), InRange(0, 9), InRange(0, 18))),
+        (1, ExerciseTemplate(Operation.addition, InRange(0, 10), InRange(0, 10), InRange(0, 20))),
+        (1, ExerciseTemplate(Operation.addition, InRange(6, 9), InRange(6, 9), InRange(12, 18))),
         (1, ExerciseTemplate(Operation.subtraction, InRange(0, 9), InRange(0, 9), InRange(0, 9))),
     )),
 )
