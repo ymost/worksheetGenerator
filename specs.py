6726fccb6dcd78ffb8cb3be9222ea18508c791a5
Yonatan Most <>
2020-05-09 20:09:39 +0300

    Update spec

--- specs.py
+++ specs.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-from constraints import InRange, Even, NoConstraint
+from constraints import InRange, NoConstraint
 from exercise_template import ExerciseTemplate
 from operation import Operation
 from worksheet_spec import WorksheetSpec
@@ -11,7 +11,7 @@ SPECS: Tuple[WorksheetSpec, ...] = (
         (1, ExerciseTemplate(Operation.subtraction, InRange(0, 15), InRange(0, 10), InRange(0, 15))),
         (0.4, ExerciseTemplate(Operation.subtraction, InRange(0, 10), InRange(0, 11), InRange(-10, -1))),
         (0.4, ExerciseTemplate(Operation.multiplication, InRange(0, 5), InRange(0, 5), InRange(0, 25))),
-        (0.2, ExerciseTemplate(Operation.division, Even(InRange(0, 20)), InRange(2, 2), NoConstraint()))
+        (0.2, ExerciseTemplate(Operation.division, InRange(0, 20), InRange(2, 2), NoConstraint()))
     )),
     WorksheetSpec('ages 3-4', (
         (1, ExerciseTemplate(Operation.addition, InRange(0, 10), InRange(0, 10), InRange(0, 20))),
